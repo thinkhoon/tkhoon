feaa0c93aa80e11eb6e5c44db50fe0527f19d72a
zengyu <a1023824314@163.com>
2016-08-10 11:34:10 +0800

    增加COOKIE操作的封装

--- src/main/java/com/tkhoon/framework/DataContext.java
+++ src/main/java/com/tkhoon/framework/DataContext.java
@@ -1,10 +1,11 @@
 package com.tkhoon.framework;
 
+import com.tkhoon.framework.util.ArrayUtil;
 import com.tkhoon.framework.util.CastUtil;
+import com.tkhoon.framework.util.CodecUtil;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -45,14 +46,14 @@ public class DataContext {
     }
 
     // 获取 Servlet Context
-    private static ServletContext getServletContext() {
+    private static javax.servlet.ServletContext getServletContext() {
         return getRequest().getServletContext();
     }
 
     // 封装 Request 相关操作
     public static class Request {
 
-        // 将数据放入 Request Attribute 中
+        // 将数据放入 Request 中
         public static void put(String key, Object value) {
             getRequest().setAttribute(key, value);
         }
@@ -83,18 +84,18 @@ public class DataContext {
     // 封装 Response 相关操作
     public static class Response {
 
-        // 将数据放入 Response Header 中
+        // 将数据放入 Response 中
         public static void put(String key, Object value) {
             getResponse().setHeader(key, CastUtil.castString(value));
         }
 
-        // 从 Response Header 中获取数据
+        // 从 Response 中获取数据
         @SuppressWarnings("unchecked")
         public static <T> T get(String key) {
             return (T) getResponse().getHeader(key);
         }
 
-        // 从 Response Header 中获取所有数据
+        // 从 Response 中获取所有数据
         public static Map<String, Object> getAll() {
             Map<String, Object> map = new HashMap<String, Object>();
             for (String name : getResponse().getHeaderNames()) {
@@ -107,23 +108,23 @@ public class DataContext {
     // 封装 Session 相关操作
     public static class Session {
 
-        // 将数据放入 Session Attribute 中
+        // 将数据放入 Session 中
         public static void put(String key, Object value) {
             getSession().setAttribute(key, value);
         }
 
-        // 从 Session Attribute 中获取数据
+        // 从 Session 中获取数据
         @SuppressWarnings("unchecked")
         public static <T> T get(String key) {
             return (T) getSession().getAttribute(key);
         }
 
-        // 移除 Session Attribute 中的数据
+        // 移除 Session 中的数据
         public static void remove(String key) {
             getSession().removeAttribute(key);
         }
 
-        // 从 Session Attribute 中获取所有数据
+        // 从 Session 中获取所有数据
         public static Map<String, Object> getAll() {
             Map<String, Object> map = new HashMap<String, Object>();
             Enumeration<String> names = getSession().getAttributeNames();
@@ -134,32 +135,71 @@ public class DataContext {
             return map;
         }
 
-        // 移除 Session Attribute 中所有的数据
+        // 移除 Session 中所有的数据
         public static void removeAll() {
             getSession().invalidate();
         }
     }
 
-    // 封装 Servlet Context 相关操作
-    public static class Context {
+    // 封装 Cookie 相关操作
+    public static class Cookie {
 
-        // 将数据放入 Servlet Context 中
+        // 将数据放入 Cookie 中
+        public static void put(String key, Object value) {
+            String strValue = CodecUtil.encodeForUTF8(CastUtil.castString(value));
+            javax.servlet.http.Cookie cookie = new javax.servlet.http.Cookie(key, strValue);
+            getResponse().addCookie(cookie);
+        }
+
+        // 从 Cookie 中获取数据
+        @SuppressWarnings("unchecked")
+        public static <T> T get(String key) {
+            T value = null;
+            javax.servlet.http.Cookie[] cookieArray = getRequest().getCookies();
+            if (ArrayUtil.isNotEmpty(cookieArray)) {
+                for (javax.servlet.http.Cookie cookie : cookieArray) {
+                    if (key.equals(cookie.getName())) {
+                        value = (T) CodecUtil.decodeForUTF8(cookie.getValue());
+                        break;
+                    }
+                }
+            }
+            return value;
+        }
+
+        // 从 Cookie 中获取所有数据
+        public static Map<String, Object> getAll() {
+            Map<String, Object> map = new HashMap<String, Object>();
+            javax.servlet.http.Cookie[] cookieArray = getRequest().getCookies();
+            if (ArrayUtil.isNotEmpty(cookieArray)) {
+                for (javax.servlet.http.Cookie cookie : cookieArray) {
+                    map.put(cookie.getName(), cookie.getValue());
+                }
+            }
+            return map;
+        }
+    }
+
+    // 封装 ServletContext 相关操作
+    public static class ServletContext {
+
+        // 将数据放入 ServletContext 中
         public static void put(String key, Object value) {
             getServletContext().setAttribute(key, value);
         }
 
-        // 从 Servlet Context 中获取数据
+        // 从 ServletContext 中获取数据
         @SuppressWarnings("unchecked")
         public static <T> T get(String key) {
             return (T) getServletContext().getAttribute(key);
         }
 
-        // 移除 Servlet Context 中的数据
+        // 移除 ServletContext 中的数据
         public static void remove(String key) {
             getServletContext().removeAttribute(key);
         }
 
-        // 从 Servlet Context 中获取所有数据
+        // 从 ServletContext 中获取所有数据
         public static Map<String, Object> getAll() {
             Map<String, Object> map = new HashMap<String, Object>();
             Enumeration<String> names = getServletContext().getAttributeNames();
