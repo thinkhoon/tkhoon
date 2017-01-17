ffdada0efdd56ebd035a830b7432703618768984
thinkhoon <1023824314@qq.com>
2017-01-17 23:54:06 +0800

    添加注释

--- src/main/java/com/tkhoon/framework/ContainerListener.java
+++ src/main/java/com/tkhoon/framework/ContainerListener.java
@@ -11,9 +11,9 @@ public class ContainerListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        // 初始化 Helper 类型
+        // 初始化 Helper 类
         tkhoon.init();
-        // 添加 Servlet 映射  
+        // 添加 Servlet 映射
         addServletMapping(sce.getServletContext());
     }
 
