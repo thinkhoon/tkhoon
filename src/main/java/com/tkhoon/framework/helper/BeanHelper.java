36f97f9e5f5ff1db5ba3563bc9970bda7036750e
thinkhoon <1023824314@qq.com>
2017-08-07 10:20:56 +0800

    移除VelocityUtil

--- src/main/java/com/tkhoon/framework/helper/BeanHelper.java
+++ src/main/java/com/tkhoon/framework/helper/BeanHelper.java
@@ -4,7 +4,6 @@ import com.tkhoon.framework.annotation.Bean;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.log4j.Logger;
 
 public class BeanHelper {
--- src/main/java/com/tkhoon/framework/util/FileUtil.java
+++ src/main/java/com/tkhoon/framework/util/FileUtil.java
@@ -1,8 +1,15 @@
 package com.tkhoon.framework.util;
 
+import com.tkhoon.framework.Constant;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Properties;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
@@ -147,6 +154,34 @@ public class FileUtil {
         }
         if (!src.isFile()) {
             throw new RuntimeException("该路径不是文件！");
+        }
+    }
+
+    // 将字符串写入文件
+    public static void writeFile(String filePath, String fileContent) {
+        OutputStream os = null;
+        Writer w = null;
+        try {
+            FileUtil.createFile(filePath);
+            os = new BufferedOutputStream(new FileOutputStream(filePath));
+            w = new OutputStreamWriter(os, Constant.DEFAULT_CHARSET);
+            w.write(fileContent);
+            w.flush();
+        } catch (Exception e) {
+            logger.error("写入文件出错！", e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+                if (w != null) {
+                    w.close();
+                }
+            } catch (Exception e) {
+                logger.error("释放资源出错！", e);
+                e.printStackTrace();
+            }
         }
     }
 }
