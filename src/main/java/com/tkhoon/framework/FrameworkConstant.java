d8be1a6403504b6e455a91f190d2800025ea66f0
thinkhoon <1023824314@qq.com>
2017-09-11 19:57:11 +0800

    fix bug

--- src/main/java/com/tkhoon/framework/FrameworkConstant.java
+++ src/main/java/com/tkhoon/framework/FrameworkConstant.java
@@ -10,9 +10,9 @@ public interface FrameworkConstant {
 
     String DEFAULT_SERVLET_NAME = "default";  
     String JSP_SERVLET_NAME = "jsp";
-    String UPLOAD_SERVLET_NAME = "upload";  
+    String UPLOAD_SERVLET_NAME = "upload";
   
-    String FAVICON_ICO_URL = "/favicon.ico";
+    String FAVICON_ICO_URL = "/favicon.ico";  
     String UPLOAD_SERVLET_URL = "/upload.do";
 
     String UPLOAD_BASE_PATH = "upload/";
